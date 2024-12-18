a3bfc189b6814a6f9e5062c0eee1e627692f8b32
Jakub Nahacz <jakubnahacz@gmail.com>
2024-12-18 18:29:19 +0100

    general improvements

--- JN_PROJEKT/my_student.c
+++ JN_PROJEKT/my_student.c
@@ -10,19 +10,21 @@ MyStudent* my_student_create(char* surname, int birth_year, FieldOfStudy field_o
 		exit(EXIT_FAILURE);
 	}
 
-	size_t length = strlen(surname);
+	size_t surname_length = strlen(surname);
 
-	student->surname = malloc(sizeof(char) * length + 1);
+	student->surname = malloc(sizeof(char) * surname_length + 1);
 
 	if (!student->surname) {
 		exit(EXIT_FAILURE); // TODO error handling
 	}
 
-	strcpy_s(student->surname, length + 1, surname);
+	strcpy_s(student->surname, surname_length + 1, surname);
 
 	student->birth_year = birth_year;
 
 	student->field_of_study = field_of_study;
+
+	return student;
 }
 
 void my_student_serialize(MyStudent* student, struct Buffer* output) {
